82dec5d3cc7b8440905404e6fcd8ec45d4491408
okantastan <okantastan1196@gmail.com>
2022-02-22 12:31:54 +0300

    added function response type

--- app/Http/Controllers/RatingController.php
+++ app/Http/Controllers/RatingController.php
@@ -20,10 +20,9 @@ class RatingController extends Controller
     /**
      * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
      */
-    public function get_ratings()
+    public function get_ratings(): string
     {
-        if($this->token == $request->input('token'))
-        {
+        if($this->token == $request->input('token')){
             $data = Ratings::all();
             return response()->json($data);
         }else{
@@ -37,30 +36,27 @@ class RatingController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function add_rating(Request $request)
+    public function add_rating(Request $request): string
     {
-        if($this->token == $request->input('token'))
-        {
+        if($this->token == $request->input('token')){
             $validator = Validator::make($request->all(),[
                 'post_id'       => 'required|int',
                 'post_title'    => 'required|string',
-                'rating'        => 'required|int',
+                'rate'          => 'required|int',
                 'token'         => 'required|string'
             ]);
             
-            if($validator->fails())
-            {
+            if($validator->fails()){
                 return response()->json($validator->errors());   
             }else{
                 Ratings::insert([
                     'post_id'    => $request->input('post_id'),
                     'post_title' => $request->input('post_title'),
-                    'rating'     => $request->input('rating'),
-                    'token'      => $request->input('token')
+                    'rate'       => $request->input('rate')
                 ]);
+                return response()->json('Rating score successfully received.'); 
             }
-        }else{
-            return response()->json('The token value is invalid.'); 
         }
+        return response()->json('The token value is invalid.');
     }
 }
